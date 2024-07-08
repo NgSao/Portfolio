2a9860c485273ac521018acd4c0c1024366f1184
NgSao <nguyensaovn2019@gmail.com>
2024-07-08 13:04:08 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+var New= document.querySelector(".New");
+var nav= document.querySelector(".nav"); 
+var links= document.querySelector(".nav 11");
+New.addEventListener("click", function () { this.classList.New("click"); nav.classList.New("open");
+})
